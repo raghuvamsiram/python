1d8b7e55058e50504a8a1180ff6ec7052250c042
raghuvamsiram <raghuvamsiram@gmail.com>
2022-10-23 09:47:57 -0700

    Refactored for shorthand
    Readable print statements for output.

--- AgeCalculator.py
+++ AgeCalculator.py
@@ -1,21 +1,17 @@
 from datetime import date
 
 def ageCalculator(currentDate, birthDate):
-    age = currentDate.year - birthDate.year
-    if (currentDate.month, currentDate.day) < (birthDate.month, birthDate.day):
-        age = age - 1
-    print(age)
+    adjustedAge = (currentDate.month, currentDate.day) < (birthDate.month, birthDate.day)
+    age = currentDate.year - birthDate.year - adjustedAge
+    return age
 
 def main():
     currentDate = date.today()
     birthDate = date(1989,5,7)    #date(yyyy,m,d)
     futureDate = date(2032,7,1)
 
-    print("Age as of date - ", currentDate)
-    ageCalculator(currentDate, birthDate)
-
-    print("Age as of date - ", futureDate)
-    ageCalculator(futureDate, birthDate)
+    print("Age as of date - ", currentDate, " is ", ageCalculator(currentDate, birthDate))
+    print("Age as of date - ", futureDate, " is ", ageCalculator(futureDate, birthDate))
 
 if __name__ == '__main__':
     main()
